3029741ba62e3482d408ade6dc2809ed4cdaa58f
ChrisSmith2 <9710782+ChrisSmith2@users.noreply.github.com>
2021-10-30 15:07:27 -0500

    Setup handlebars

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
+handlebars = exphbs.create({
+    defaultLayout: 'main',
+    extname: '.html'
+});
+
+app.engine('html', handlebars.engine);
+app.set('view engine', 'html');
+
+app.get('/', (req, res) => {
+    res.render('index', {layout: false});
+});
+
+app.get('/preferences', (req, res) => {
+    res.render('preferences', {layout: false});
+});
+
+app.engine('html', handlebars.engine);
+app.set('view engine', 'html');
+
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
-});
+}); 
