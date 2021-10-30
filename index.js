ce6bbc0c388298c914c67635c8c0b295e1282dc3
ChrisSmith2 <9710782+ChrisSmith2@users.noreply.github.com>
2021-10-30 12:51:08 -0500

    Update port

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
-const port = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
-app.listen(port, () => {
+app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${port}`);
 });
